561739cd1b3387d2ff53944c5fb8973401c17ce5
Jake Crane <jake@jakecrane.com>
2016-10-30 14:55:49 -0500

    Fixed Input Validation Error Messages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+I avoided using JQuery in this particular project just to see what it was like.
 A working version of this Project can be found [here](http://jakecrane.com/LemonadeCalculator/).
--- src/js/lemonade.js
+++ src/js/lemonade.js
@@ -2,17 +2,36 @@ var lemonRatioInput = document.getElementById("lemonRatio");
 var sugarRatioInput = document.getElementById("sugarRatio");
 var waterRatioInput = document.getElementById("waterRatio");
 var cupsOfLemonJuiceInput = document.getElementById("cupsOfLemonJuice");
+var sugarResultElement = document.getElementById("sugarResult");
+var waterResultElement = document.getElementById("waterResult");
+var errorSpan = document.getElementById("errorMessage");
+var errorDiv = document.querySelector(".alert.alert-danger");
+var hideErrorLink = document.querySelector('.alert.alert-danger>a');
+
 lemonRatioInput.onkeyup = update;
 sugarRatioInput.onkeyup = update;
 waterRatioInput.onkeyup = update;
 cupsOfLemonJuiceInput.onkeyup = update;
 
-var sugarResultElement = document.getElementById("sugarResult");
-var waterResultElement = document.getElementById("waterResult");
+hideErrorLink.onclick = hideError;
+
+function hideError(e) {
+	errorDiv.classList.add('collapse');
+}
+
+function showError(errorMessage) {
+	sugarResultElement.innerHTML = '';
+	waterResultElement.innerHTML = '';
+	errorSpan.innerHTML = errorMessage;
+	errorDiv.classList.remove('collapse');
+}
 
 function update(e) {
-	if (e.which === 13)
-		$(this).blur();
+	if (e.which === 13) {
+		this.blur();
+		return;
+	}
+
 	var lemonRatio = parseFloat(lemonRatioInput.value);
 	var sugarRatio = parseFloat(sugarRatioInput.value);
 	var waterRatio = parseFloat(waterRatioInput.value);
@@ -20,12 +39,13 @@ function update(e) {
 	
 	var ratioSum = lemonRatio + sugarRatio + waterRatio;
 	if (ratioSum < .999 || ratioSum > 1) {
-		resultElement.innerHTML = "Ratios do not add up to 1.";
+		showError('Ratios do not add up to 1.');
 	} else if (!lemonRatio || !sugarRatio || !waterRatio || !cupsOfLemonJuice) {
-		resultElement.innerHTML = "Invalid input";
+		showError('Invalid or missing input.');
 	} else {
 		sugarResultElement.innerHTML = parseFloat(cupsOfSugar(cupsOfLemonJuice, lemonRatio, sugarRatio)).toFixed(1);
 		waterResultElement.innerHTML = parseFloat(cupsOfWater(cupsOfLemonJuice, lemonRatio, waterRatio)).toFixed(1);
+		hideError();
 	}
 }
 
